560d4b6bb3690690447d2a884d792d24c7d2b617
libin581 <libin_581@163.com>
2018-07-16 09:17:12 +0800

    add files

--- work_used_scripts/decode_dcd_fieldname_fill.py
+++ work_used_scripts/decode_dcd_fieldname_fill.py
@@ -16,14 +16,14 @@ def usage():
     decode_dcd_fieldname_fill.py            : help info
     decode_dcd_fieldname_fill.py -h         : help info
     decode_dcd_fieldname_fill.py --help     : help info
-    decode_dcd_fieldname_fill.py *.dcd -c   : check decode template
-    decode_dcd_fieldname_fill.py *.dcd -c   : audo fill decode template
+    decode_dcd_fieldname_fill.py -c *.dcd   : check decode template
+    decode_dcd_fieldname_fill.py -f *.dcd   : audo fill decode template
     decode_dcd_fieldname_fill.py path       : check and fill all decode templates at this path
     
     eg: 
-       1. decode_dcd_fieldname_fill.py file.dcd -c
-       2. decode_dcd_fieldname_fill.py file.dcd -f
-       3. decode_dcd_fieldname_fill.py ./ -c
+       1. decode_dcd_fieldname_fill.py -c file.dcd
+       2. decode_dcd_fieldname_fill.py -f file.dcd
+       3. decode_dcd_fieldname_fill.py -c ./ 
     
     note:
     dcd文件应该有完整的FIELD_NAME和FIELD_NEXT_FIELD结构
@@ -37,160 +37,216 @@ def isDcdFile(fileName):
     else:
         return False 
     
-comment_line_pat = re.compile(r"^\s*//")
-def skipCommentLine(line):
-    if comment_line_pat.search(line):
+blank_line_pat = re.compile(r"^\s*$")
+def skipBlankLine(line):
+    if blank_line_pat.search(line):
         return True
     else:
         return False
     
+comment_pat = re.compile(r"\/\/")
+def removeComment(line):
+    if comment_pat.search(line):
+        #pdb.set_trace()
+        (s,e)=comment_pat.search(line).span()
+        line = line[:s]
+    return line
+    
+def removeChangeLine(line):
+    line = line.replace("\r\n","")
+    line = line.replace("\n","")
+    return line
+    
 #define 正则表达式
-RECORD_pat        = re.compile(r"^\s+RECORD\s*\n$|^\s+RECORD\s*\r\n$") #	RECORD
-FIELD_NAME_pat    = re.compile(r"^\s+FIELD_NAME\s*=\s*\w+\s*\n$|^\s+FIELD_NAME\s*=\s*\w+\s*\r\n$") #			FIELD_NAME = BEGIN
-FIELD_NEXT_FIELD_pat = re.compile(r"^\s+FIELD_NEXT_FIELD\s*=") #			FIELD_NEXT_FIELD =
-left_bracket_pat  = re.compile(r"^\s+{")  #	{
-right_bracket_pat = re.compile(r"^\s+}")  #	{
-RECORD_NAME_pat   = re.compile(r"^\s+RECORD_NAME\s*=\s*\w+\s*\n$|^\s+RECORD_NAME\s*=\s*\w+\s*\r\n$") #			RECORD_NAME = control
+RECORD_pat        = re.compile(r"^RECORD$") #    RECORD
+FIELD_pat         = re.compile(r"^FIELD$") # FIELD
+FIELD_NAME_pat    = re.compile(r"^FIELD_NAME\s*=\s*\w+$") #            FIELD_NAME = BEGIN
+FIELD_NEXT_FIELD_pat = re.compile(r"^FIELD_NEXT_FIELD\s*=\s*\w+$") #            FIELD_NEXT_FIELD =
+FIELD_LEAF_pat    = re.compile(r"^FIELD_LEAF\s*=\s*YES$|^FIELD_LEAF\s*=\s*NO$")
+DECODE_pat        = re.compile(r"^DECODE$")
+left_bracket_pat  = re.compile(r"^{")  # {
+right_bracket_pat = re.compile(r"^}")  # {
+RECORD_NAME_pat   = re.compile(r"^RECORD_NAME\s*=\s*\w+$") #          RECORD_NAME = control
+RECORD_TYPE_pat   = re.compile(r"^RECORD_TYPE\s*=\s*\w+$")
 ASN_BER_pat           = re.compile(r"ASN|PROPERTY_FILE_ENCODE_TYPE\s*=\s*BER")
+ENCODE_TYPE_pat   = re.compile(r"PROPERTY_FILE_ENCODE_TYPE\s*=\s*\w+$")
 var_pat = re.compile(r"\w+")
 
-FIELDNAME_count = {}
-def FIELD_NAME_cout(line_list):
-    #count FIELD_NAME in RECORD 
-    IS_IN_RECORD      = False
-    bracket_deep      = 0
-    FIELD_NAME_num    = 0
-    RECORD_NAME_value = []
-    for i, line in enumerate(line_list):
-        if skipCommentLine(line) == True:
+isInRecord      = False
+bracket_deep      = 0
+fieldNameCount  = 0
+RECORD_NAME = ""
+RECORD_TYPE = ""
+FIELD_NAME = ""
+FIELD_NEXT_FIELD = ""
+def recordVarsInit():
+    global isInRecord 
+    global bracket_deep
+    global fieldNameCount
+    global RECORD_NAME
+    global RECORD_TYPE
+    global FIELD_NAME
+    global FIELD_NEXT_FIELD
+    isInRecord       = False
+    bracket_deep     = 0
+    fieldNameCount   = 0
+    RECORD_NAME      = ""
+    RECORD_TYPE      = ""
+    FIELD_NAME       = ""
+    FIELD_NEXT_FIELD = ""
+
+isInField    = False
+hasFieldLeaf = False
+hasFieldName = False
+hasFieldNextField = False
+hasDecode    = False
+def fieldVarsInit():
+    global isInField
+    global hasFieldLeaf
+    global hasFieldName
+    global hasFieldNextField
+    global hasDecode
+    isInField    = False
+    hasFieldLeaf = False
+    hasFieldName = False
+    hasFieldNextField = False
+    hasDecode    = False
+
+
+def checkField(lines):
+    global isInRecord 
+    global bracket_deep
+    global fieldNameCount
+    global RECORD_NAME
+    global FIELD_NAME
+    global RECORD_TYPE
+    global FIELD_NEXT_FIELD
+    global isInField
+    global hasFieldLeaf
+    global hasFieldName
+    global hasFieldNextField
+    global hasDecode
+
+    recordVarsInit()
+    fieldVarsInit()
+    encodeType = ""
+    for i, line in enumerate(lines):
+        i+=1
+        line = removeComment(line)
+        line = removeChangeLine(line)
+        line = line.strip()
+        #pdb.set_trace()
+        if skipBlankLine(line) == True:
             continue
            
-        if IS_IN_RECORD == False:
+        if not encodeType:
+            if ENCODE_TYPE_pat.search(line):
+                #pdb.set_trace()
+                vars = line.split("=")
+                encodeType = vars[-1].strip()
+        else:
+            if not encodeType == "ASCII":
+                print "file is not ASC encode"
+                break;
+           
+        if isInRecord == False:
             if RECORD_pat.search(line):
-                IS_IN_RECORD = True
+                isInRecord = True
             continue
         else:
             if left_bracket_pat.search(line):
                 bracket_deep += 1
-                continue
+
             if right_bracket_pat.search(line):
                 bracket_deep -= 1
-                if bracket_deep == 0:
-                    IS_IN_RECORD = False
-                    if RECORD_NAME_value:
-                        FIELDNAME_count[ RECORD_NAME_value] = FIELD_NAME_num
-                        FIELD_NAME_num = 0
-                        RECORD_NAME_value = []
-                    else:
-                        print "RECORD_NAME is null!!!"
-                        sys.exit()
-                continue
-            FIELD_NAME_search = FIELD_NAME_pat.search(line)
-            if  FIELD_NAME_search:
-                FIELD_NAME_num += 1
-            RECORD_NAME_search = RECORD_NAME_pat.search(line)
-            if RECORD_NAME_search:
-                vars = var_pat.findall(line)
-                RECORD_NAME_value = vars[-1]
-                            
-    print
-    print "-----RECORD_NAME:  FIELD count-----"
-    for FIELD_NAME in FIELDNAME_count.keys():
-        print "%s: %d"%(FIELD_NAME, FIELDNAME_count[FIELD_NAME])
-    print
-    
-    
-#check all FIELD_NAME and FIELD_NEXT_FIELD
-def check_FIELD_NAME(line_list):
-    IS_IN_RECORD      = False
-    bracket_deep      = 0
-    FIELD_NAME_num    = 0
-    FIELD_NEXT_FIELD = []
-    RECORD_NAME_value = []
-    FIELD_count         = 0
-    line_count = len(line_list)
-    
-    print "-----check begin-----"
-    
-    for i, line in enumerate(line_list):
-        if i == (line_count-1):
-            print "no error found"
-            print
-            continue
-          
-        if ASN_BER_pat.search(line):
-            return False
-            
-        if skipCommentLine(line) == True:
-            continue
-           
-        if IS_IN_RECORD == False:
-            if RECORD_pat.search(line):
-                IS_IN_RECORD = True
-            continue
-        else:
-            if left_bracket_pat.search(line):
-                bracket_deep += 1
-                continue
-            if right_bracket_pat.search(line):
-                bracket_deep -= 1
-                if bracket_deep == 0:
-                    IS_IN_RECORD      = False
-                    bracket_deep      = 0
-                    FIELD_NAME_num    = 0
-                    FIELD_NEXT_FIELD = []
-                    RECORD_NAME_value = []
-                continue
                 
-            if RECORD_NAME_value == []:    
-                RECORD_NAME_search = RECORD_NAME_pat.search(line)
-                if RECORD_NAME_search:
-                    vars = var_pat.findall(line)
-                    RECORD_NAME_value = vars[-1]
-                    if FIELDNAME_count.has_key(RECORD_NAME_value):
-                        FIELD_count = FIELDNAME_count[RECORD_NAME_value]
-                    else: 
-                        print "RECORD_NAME is not found!!!"
-                        sys.exit()
-                    continue
-                
-            FIELD_NAME_search = FIELD_NAME_pat.search(line)
-            if  FIELD_NAME_search:
-                FIELD_NAME_num += 1
-                if not (FIELD_NAME_num == 1):
-                    if FIELD_NEXT_FIELD:
+            if bracket_deep == 0: #the end of record
+                #pdb.set_trace()
+                if RECORD_NAME:
+                    print "RECORD_NAME:%s, count:%d"%(RECORD_NAME, fieldNameCount)
+                    print
+                else:
+                    print "line: %d RECORD_NAME is null!!!"%(i)
+                    sys.exit()
+                if not RECORD_TYPE:
+                    print "line: %d RECORD_TYPE is null!!!"%(i)
+                    #sys.exit()
+                if not (RECORD_TYPE == "TAIL" or RECORD_TYPE == "HEAD") and not FIELD_NEXT_FIELD == "NULL":
+                    print "line %d, the last FIELD_NEXT_FIELD is not NULL"%(i)
+                    sys.exit()
+                    
+                recordVarsInit()
+                fieldVarsInit()
+            elif bracket_deep == 1:
+                if not RECORD_NAME:
+                    RECORD_NAME_search = RECORD_NAME_pat.search(line)
+                    if RECORD_NAME_search:
                         vars = var_pat.findall(line)
-                        FIELD_NAME_value = vars[-1]
-                        if not (FIELD_NAME_value == FIELD_NEXT_FIELD):
-                            print "line number:%d, FIELD_NAME:%s, FIELD_NEXT_FIELD:%s"%(i+1, FIELD_NAME_value, FIELD_NEXT_FIELD)
+                        RECORD_NAME = vars[-1]
+                if not RECORD_TYPE:
+                    RECORD_TYPE_search = RECORD_TYPE_pat.search(line)
+                    if RECORD_TYPE_search:
+                        vars = var_pat.findall(line)
+                        RECORD_TYPE = vars[-1]
+                if isInField == True:
+                    if hasFieldName == False:
+                        print "line %d has not correct FIELD_NAME"%(i)
+                        sys.exit()
+                    if hasFieldNextField == False:
+                        print "line %d has not correct FIELD_NEXT_FIELD"%(i)
+                        sys.exit()
+                    if hasFieldLeaf == False:
+                        print "line %d has not correct FIELD_LEAF"%(i)
+                    if hasDecode == False:
+                        print "line %d has not correct DECODE"%(i)
+                        sys.exit()
+                    if fieldNameCount == 1:
+                        if not FIELD_NAME == "BEGIN":
+                            print "line %s, the first FIELD_NAME must be 'BEGIN' "%(i)
                             sys.exit()
-                    else:
-                        print "FIELD_NEXT_FIELD is null!!!"
-                        sys.exit()
-                continue
-                
-            FIELD_NEXT_FIELD_search = FIELD_NEXT_FIELD_pat.search(line)
-            if FIELD_NEXT_FIELD_search:
-                vars = var_pat.findall(line)
-                FIELD_NEXT_FIELD = vars[-1]
-                if FIELD_NAME_num == FIELD_count:
-                    if not(FIELD_NEXT_FIELD == "NULL"):
-                        print "line number:%d, the last FIELD_NEXT_FIELD:%s"%(i+1, FIELD_NEXT_FIELD) 
-                        sys.exit()
+                    fieldVarsInit()
+            elif bracket_deep == 2:
+                isInField = True
+                FIELD_NAME_search = FIELD_NAME_pat.search(line)
+                if  FIELD_NAME_search:
+                    fieldNameCount += 1
+                    hasFieldName = True
+                    vars = var_pat.findall(line)
+                    FIELD_NAME = vars[-1]
+                    if not FIELD_NAME == "BEGIN":
+                        if not FIELD_NEXT_FIELD and not FIELD_NAME == FIELD_NEXT_FIELD:
+                            print "line %d FIELD_NAME is not right"%(i)
+                            sys.exit()
+                FIELD_LEAF_search = FIELD_LEAF_pat.search(line)
+                #pdb.set_trace()
+                if FIELD_LEAF_search:
+                    #pdb.set_trace()
+                    hasFieldLeaf = True
+                FIELD_NEXT_FIELD_search = FIELD_NEXT_FIELD_pat.search(line)
+                if FIELD_NEXT_FIELD_search:
+                    hasFieldNextField = True
+                    vars = var_pat.findall(line)
+                    FIELD_NEXT_FIELD = vars[-1]
+                DECODE_search = DECODE_pat.search(line)
+                if DECODE_search:
+                    hasDecode = True
+            elif bracket_deep < 0:
+                print "right bracket is more than left bracket"
+                sys.exit()
     
 #fill all FIELD_NAME and FIELD_NEXT_FIELD
-def fill_FIELD_NAME(line_list):
-    IS_IN_RECORD      = False
+def fillField(lines):
+    isInRecord      = False
     bracket_deep      = 0
-    FIELD_NAME_num    = 0
+    fieldNameCount    = 0
     FIELD_NEXT_FIELD = []
-    for i, line in enumerate(line_list):
+    for i, line in enumerate(lines):
         if skipCommentLine(line) == True:
             continue
            
-        if IS_IN_RECORD == False:
+        if isInRecord == False:
             if RECORD_pat.search(line):
-                IS_IN_RECORD = True
+                isInRecord = True
             continue
         else:
             if left_bracket_pat.search(line):
@@ -199,7 +255,7 @@ def fill_FIELD_NAME(line_list):
             if right_bracket_pat.search(line):
                 bracket_deep -= 1
                 if bracket_deep == 0:
-                    IS_IN_RECORD = False
+                    isInRecord = False
                 continue
                 
             RECORD_NAME_search = RECORD_NAME_pat.search(line)
@@ -207,7 +263,7 @@ def fill_FIELD_NAME(line_list):
                 vars = var_pat.findall(line)
                 RECORD_NAME_value = vars[-1]
                 if FIELDNAME_count.has_key(RECORD_NAME_value):
-                    FIELD_NAME_num = FIELDNAME_count[RECORD_NAME_value]
+                    fieldNameCount = FIELDNAME_count[RECORD_NAME_value]
                 else: 
                     print "RECORD_NAME is not found!!!"
                     sys.exit()
@@ -215,8 +271,8 @@ def fill_FIELD_NAME(line_list):
                 
             FIELD_NAME_search = FIELD_NAME_pat.search(line)
             if  FIELD_NAME_search:
-                FIELD_NAME_num += 1
-                if FIELD_NAME_num == 1: 
+                fieldNameCount += 1
+                if fieldNameCount == 1: 
                     line = FIELD_NAME_search.group + "BEGIN" + "\n"
                 else:
                     if FIELD_NEXT_FIELD:
@@ -226,53 +282,54 @@ def fill_FIELD_NAME(line_list):
                         sys.exit()
     
 #------------main func-------------------------------------
-file = ""
-if len(sys.argv) == 1:
-    usage()
-    sys.exit()
-elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    usage()
-    sys.exit()
-else:
-    file = sys.argv[1]
+def main():
+    file = ""
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit()
+    elif len(sys.argv) == 2 and (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+        usage()
+        sys.exit()
+    elif len(sys.argv) == 3 :
+        file = sys.argv[2]
+    else:
+        print "error input parameters"
+        usage()
 
-option = "-f"
-if len(sys.argv) == 3:
-    if sys.argv[2] == "-c" or sys.argv[2] == "--check":
-        option = "-c"
-    elif sys.argv[2] == "-f" or sys.argv[2] == "--fill":
-        option = "-f"
+    option = ""
+    if len(sys.argv) == 3:
+        if sys.argv[1] == "-c" or sys.argv[1] == "--check":
+            option = "-c"
+        elif sys.argv[1] == "-f" or sys.argv[1] == "--fill":
+            option = "-f"
+            
         
-    
-file_list=[]
-if os.path.isfile(file):
-    file_list.append(file)
-elif os.path.isdir(file):
-    workPath=file
-    for  root, dirs,files in os.walk(workPath):
-        for file in files:
-            if isDcdFile(file):
-                file_list.append(root+"/"+file)
-else:
-    print "%s is not file or dir!!!"%(file)
+    file_list=[]
+    if os.path.isfile(file):
+        file_list.append(file)
+    elif os.path.isdir(file):
+        workPath=file
+        for  root, dirs,files in os.walk(workPath):
+            for file in files:
+                if isDcdFile(file):
+                    file_list.append(root+file)
+    else:
+        print "%s is not file or dir!!!"%(file)
 
-for file in file_list:
+    for file in file_list:
 
-    print "=====file:%s====="%(file)
+        print "=====file:%s====="%(file)
 
-    line_list = []
-    READ_FILE   = open(file, "r")
-    line = READ_FILE.readline()
-    while(line):
-        line_list.append(line)
-        line = READ_FILE.readline()
-    READ_FILE.close()
-   
-    FIELD_NAME_cout(line_list)
-    
-    check_FIELD_NAME(line_list)
+        READ_FILE   = open(file, "r")
+        lines = READ_FILE.readlines()
+        READ_FILE.close()
+       
+        if option == "-c":
+            #pdb.set_trace()
+            checkField(lines)
                     
-
+if __name__ == '__main__':
+    main()
                  
                         
     
---
+++ work_used_scripts/getTag.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import pdb
+
+def usage():
+    print '''
+    getTag.py + 十六进制的tag值
+    eg:
+        getTag.py 5F8101 #129
+      
+    '''
+
+def getbits(x, p, n):
+    x,p,n=int(x,16),int(p),int(n)
+    return ( int(x) >> (p+1-n))  & ~(~0 << n);
+
+def main():
+    '''
+    把十六进制的tag，转成十进制tag
+    '''
+
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit()
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+         usage()
+         sys.exit()
+    else:
+         tagHex = sys.argv.pop()
+
+    tagx = tagHex[0:2]
+    ident = getbits(tagx,4,5)
+    if (ident == 31):
+        tag2x = tagHex[2:4]
+        if not tag2x:
+            return
+        ident = getbits(tag2x, 6, 7)
+        ii=4
+        while getbits(tag2x, 7,1):
+            ident <<= 7
+            tag2x = tagHex[ii:ii+2]
+            ii+=2
+            if not tag2x:
+                return
+            ident += getbits(tag2x, 6, 7)
+    print ident
+    
+if __name__ == '__main__':
+    main()
